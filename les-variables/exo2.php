8fa93c63aeda56433a01aca3b637b335ead25128
David Jegat <david.jegat@gmail.com>
2021-05-25 15:14:39 +0200

    Correction les variables exo2

---
+++ les-variables/exo2.php
@@ -0,0 +1,13 @@
+<?php
+
+// Création de la variable $nom de type string
+$nom = 'Doe';
+
+// Création de la variable $prenom de type string
+$prenom = 'John';
+
+// Création de la variable $age de type int (entier)
+$age = 30;
+
+// Nous affichons le text : « <prenom> <nom> »
+echo 'Bonjour ' . $prenom . ' ' . $nom . ', vous avez ' . $age . ' ans !';
