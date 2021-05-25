9626b65c2ffa9f692a7e2c250d0980d761df5735
David Jegat <david.jegat@gmail.com>
2021-05-25 15:11:19 +0200

    Ajout correction variable exo1

---
+++ les-variables/exo1.php
@@ -0,0 +1,10 @@
+<?php
+
+// Création de la variable $nom de type string
+$nom = 'Doe';
+
+// Création de la variable $prenom de type string
+$prenom = 'John';
+
+// Nous affichons le text : « <prenom> <nom> »
+echo $prenom . ' ' . $nom;
