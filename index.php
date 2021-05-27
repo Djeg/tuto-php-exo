f76ffa8a9982f0c430ff654d832d94f85ccc22af
David Jegat <david.jegat@gmail.com>
2021-05-27 15:17:35 +0200

    Correction - Les fonctions - exo 1

--- index.php
+++ index.php
@@ -1,55 +1,14 @@
 <?php
 
-$eleve1 = [
-    'nom' => 'Dupont',
-    'prenom' => 'Jean',
-    'age' => 12,
-    'notes' => [12, 5, 19],
-];
+function calculerMoyenne($notes)
+{
+    $total = 0;
 
-$eleve2 = [
-    'nom' => 'Dupont',
-    'prenom' => 'Jane',
-    'age' => 13,
-    'notes' => [17, 9, 12, 17],
-];
-
-$eleve3 = [
-    'nom' => 'Doe',
-    'prenom' => 'John',
-    'age' => 11,
-    'notes' => [8, 19, 16],
-];
-
-$eleves = [$eleve1, $eleve2, $eleve3];
-
-foreach ($eleves as $index => $eleve) {
-    echo '<div>';
-    echo '<p>'
-        . $eleve['nom']
-        . ' '
-        . $eleve['prenom']
-        . '</p>';
-
-    echo '<ul>';
-
-    foreach ($eleve['notes'] as $indexNote => $note) {
-        echo '<li>' . $note . ' / 20</li>';
+    foreach ($notes as $note) {
+        $total = $total + $note;
     }
 
-    echo '</ul>';
-
-    echo '</div>';
+    return $total / count($notes);
 }
 
-// foreach ($notes as $monIndex => $maNote) {
-//     echo '<p>Note n°'
-//         . $monIndex + 1
-//         . ' est de '
-//         . $maNote
-//         . ' / 20</p>';
-// }
-
-// foreach ($eleve as $key => $value) {
-//     echo '<p>' . $key . ' : ' . $value . '</p>';
-// }
+echo calculerMoyenne([13, 16, 1, 7]);
---
+++ les-fonctions/exo1.php
@@ -0,0 +1,8 @@
+<?php
+
+function titre($titre)
+{
+    return '<h1>' . $titre . '</h1>';
+}
+
+echo titre('Conseil de classe');
