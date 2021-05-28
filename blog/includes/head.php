05e2358317db74a2be49cf415178bb32620da727
David Jegat <david.jegat@gmail.com>
2021-05-28 09:56:48 +0200

    Blog - include - exo

---
+++ blog/includes/head.php
@@ -0,0 +1,9 @@
+<!--
+    1. Ici copier / coller le contenue de la balise head
+    2. Inclure ce fichier PHP dans "index.php" à la place
+       de la balise head
+    2. Inclure ce fichier PHP dans "about.php" à la place
+       de la balise head
+    2. Inclure ce fichier PHP dans "contact.php" à la place
+       de la balise head
+-->
