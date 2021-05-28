4289d6057d92f357e42eb8918ce270968ee0f96c
David Jegat <david.jegat@gmail.com>
2021-05-28 09:52:30 +0200

    Blog - inclusion - final

--- blog/index.php
+++ blog/index.php
@@ -12,20 +12,9 @@
 </head>
 
 <body>
-    <!-- Responsive navbar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#!">Mon Blog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="./index.php">Accueil</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./about.php">À propos</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./contact.php">Me contacter</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <!-- Inclusion d'un fichier PHP -->
+    <?php require('./includes/menu.php'); ?>
+
     <!-- Page header with logo and tagline-->
     <header class="py-5 bg-light border-bottom mb-4">
         <div class="container">
